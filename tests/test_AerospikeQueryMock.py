e09c7fd319f3c834e3b5f99cf8f72c8731705f72
Vit Listik <tivvit@seznam.cz>
2015-09-19 13:12:19 +0200

    reformat

--- tests/test_AerospikeQueryMock.py
+++ tests/test_AerospikeQueryMock.py
@@ -14,15 +14,21 @@ class TestAerospikeQueryMock(unittest.TestCase):
     def test_query(self):
         query = self.asm.query('a', 'b')
         query.select('a', 'c')
-        self.assertEqual([(('a', 'b', 3), {'a': 3, 'c': None}, {'gen': 1, 'ttl': 0}), (('a', 'b', 2), {'a': 2, 'c': None}, {'gen': 1, 'ttl': 0}), (('a', 'b', 1), {'a': 1, 'c': None}, {'gen': 1, 'ttl': 0})], query.results())
+        self.assertEqual(
+            [(('a', 'b', 3), {'a': 3, 'c': None}, {'gen': 1, 'ttl': 0}),
+             (('a', 'b', 2), {'a': 2, 'c': None}, {'gen': 1, 'ttl': 0}),
+             (('a', 'b', 1), {'a': 1, 'c': None}, {'gen': 1, 'ttl': 0})],
+            query.results())
 
     def test_query_namespace(self):
         query = self.asm.query('a')
         query.select('a', 'b')
-        self.assertEqual([(('a', 'b', 3), {'a': 3, 'b': 3}, {'gen': 1, 'ttl': 0}),
-            (('a', 'b', 2), {'a': 2, 'b': 2}, {'gen': 1, 'ttl': 0}),
-            (('a', 'c', 4), {'a': 4, 'b': 4}, {'gen': 1, 'ttl': 0}),
-            (('a', 'b', 1), {'a': 1, 'b': 1}, {'gen': 1, 'ttl': 0})], query.results())
+        self.assertEqual(
+            [(('a', 'b', 3), {'a': 3, 'b': 3}, {'gen': 1, 'ttl': 0}),
+             (('a', 'b', 2), {'a': 2, 'b': 2}, {'gen': 1, 'ttl': 0}),
+             (('a', 'c', 4), {'a': 4, 'b': 4}, {'gen': 1, 'ttl': 0}),
+             (('a', 'b', 1), {'a': 1, 'b': 1}, {'gen': 1, 'ttl': 0})],
+            query.results())
 
     def test_query_callback(self):
         result = []
@@ -30,23 +36,32 @@ class TestAerospikeQueryMock(unittest.TestCase):
         def callback(data):
             (key, meta, bins) = data
             result.append((key, meta, bins))
+
         query = self.asm.query('a', 'b')
         query.select('a', 'c')
         query.foreach(callback)
-        self.assertEqual([((('a', 'b', 3), {'a': 3, 'c': None}, {'gen': 1, 'ttl': 0}), (('a', 'b', 2), {'a': 2, 'c': None}, {'gen': 1, 'ttl': 0}), (('a', 'b', 1), {'a': 1, 'c': None}, {'gen': 1, 'ttl': 0}))], result)
+        self.assertEqual(
+            [((('a', 'b', 3), {'a': 3, 'c': None}, {'gen': 1, 'ttl': 0}),
+              (('a', 'b', 2), {'a': 2, 'c': None}, {'gen': 1, 'ttl': 0}),
+              (('a', 'b', 1), {'a': 1, 'c': None}, {'gen': 1, 'ttl': 0}))],
+            result)
 
     def test_query_equals(self):
         query = self.asm.query('a', 'b')
         query.select('a', 'c')
         query.where(AerospikePredicatesMock().equals("a", 1))
-        self.assertEqual([(('a', 'b', 1), {'a': 1, 'c': None}, {'gen': 1, 'ttl': 0})], query.results())
+        self.assertEqual(
+            [(('a', 'b', 1), {'a': 1, 'c': None}, {'gen': 1, 'ttl': 0})],
+            query.results())
 
     def test_query_between(self):
         query = self.asm.query('a', 'b')
         query.select('a', 'c')
         query.where(AerospikePredicatesMock().between("a", 1, 4))
-        self.assertEqual([(('a', 'b', 3), {'a': 3, 'c': None}, {'gen': 1, 'ttl': 0}),
-                          (('a', 'b', 2), {'a': 2, 'c': None}, {'gen': 1, 'ttl': 0})], query.results())
+        self.assertEqual(
+            [(('a', 'b', 3), {'a': 3, 'c': None}, {'gen': 1, 'ttl': 0}),
+             (('a', 'b', 2), {'a': 2, 'c': None}, {'gen': 1, 'ttl': 0})],
+            query.results())
 
     def test_query_contains(self):
         self.asm.put(("a", "l", 1), {"list": [1, 2, 3, 4, 5]})
@@ -54,7 +69,9 @@ class TestAerospikeQueryMock(unittest.TestCase):
         query = self.asm.query('a', 'l')
         query.select('list')
         query.where(AerospikePredicatesMock().contains("list", list, 2))
-        self.assertEqual([(('a', 'l', 1), {'list': [1, 2, 3, 4, 5]}, {'gen': 1, 'ttl': 0})], query.results())
+        self.assertEqual(
+            [(('a', 'l', 1), {'list': [1, 2, 3, 4, 5]}, {'gen': 1, 'ttl': 0})],
+            query.results())
 
     def test_query_range(self):
         self.asm.put(("a", "l", 1), {"list": [1, 2, 3, 4, 5]})
@@ -62,7 +79,10 @@ class TestAerospikeQueryMock(unittest.TestCase):
         query = self.asm.query('a', 'l')
         query.select('list')
         query.where(AerospikePredicatesMock().range("list", list, 1, 3))
-        self.assertEqual([(('a', 'l', 1), {'list': [1, 2, 3, 4, 5]}, {'gen': 1, 'ttl': 0})], query.results())
+        self.assertEqual(
+            [(('a', 'l', 1), {'list': [1, 2, 3, 4, 5]}, {'gen': 1, 'ttl': 0})],
+            query.results())
+
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_AerospikeScanMock.py
+++ tests/test_AerospikeScanMock.py
@@ -13,15 +13,24 @@ class TestAerospikeScanMock(unittest.TestCase):
     def test_scan(self):
         scan = self.asm.scan('a', 'b')
         scan.select('a', 'c')
-        self.assertEqual([(('a', 'b', 3), {'a': 3, 'c': None}, {'gen': 1, 'ttl': 0}), (('a', 'b', 2), {'a': 2, 'c': None}, {'gen': 1, 'ttl': 0}), (('a', 'b', 1), {'a': 1, 'c': None}, {'gen': 1, 'ttl': 0})], scan.results())
+        self.assertEqual([
+                             (('a', 'b', 3), {'a': 3, 'c': None},
+                              {'gen': 1, 'ttl': 0}),
+                             (('a', 'b', 2), {'a': 2, 'c': None},
+                              {'gen': 1, 'ttl': 0}),
+                             (('a', 'b', 1), {'a': 1, 'c': None},
+                              {'gen': 1, 'ttl': 0})
+                         ], scan.results())
 
     def test_scan_namespace(self):
         scan = self.asm.scan('a')
         scan.select('a', 'b')
-        self.assertEqual([(('a', 'b', 3), {'a': 3, 'b': 3}, {'gen': 1, 'ttl': 0}),
-            (('a', 'b', 2), {'a': 2, 'b': 2}, {'gen': 1, 'ttl': 0}),
-            (('a', 'c', 4), {'a': 4, 'b': 4}, {'gen': 1, 'ttl': 0}),
-            (('a', 'b', 1), {'a': 1, 'b': 1}, {'gen': 1, 'ttl': 0})], scan.results())
+        self.assertEqual(
+            [(('a', 'b', 3), {'a': 3, 'b': 3}, {'gen': 1, 'ttl': 0}),
+             (('a', 'b', 2), {'a': 2, 'b': 2}, {'gen': 1, 'ttl': 0}),
+             (('a', 'c', 4), {'a': 4, 'b': 4}, {'gen': 1, 'ttl': 0}),
+             (('a', 'b', 1), {'a': 1, 'b': 1}, {'gen': 1, 'ttl': 0})],
+            scan.results())
 
     def test_scan_callback(self):
         result = []
@@ -29,10 +38,16 @@ class TestAerospikeScanMock(unittest.TestCase):
         def callback(data):
             (key, meta, bins) = data
             result.append((key, meta, bins))
+
         scan = self.asm.scan('a', 'b')
         scan.select('a', 'c')
         scan.foreach(callback)
-        self.assertEqual([((('a', 'b', 3), {'a': 3, 'c': None}, {'gen': 1, 'ttl': 0}), (('a', 'b', 2), {'a': 2, 'c': None}, {'gen': 1, 'ttl': 0}), (('a', 'b', 1), {'a': 1, 'c': None}, {'gen': 1, 'ttl': 0}))], result)
+        self.assertEqual(
+            [((('a', 'b', 3), {'a': 3, 'c': None}, {'gen': 1, 'ttl': 0}),
+              (('a', 'b', 2), {'a': 2, 'c': None}, {'gen': 1, 'ttl': 0}),
+              (('a', 'b', 1), {'a': 1, 'c': None}, {'gen': 1, 'ttl': 0}))],
+            result)
+
 
 if __name__ == '__main__':
     unittest.main()
