e423aec7c61d81d7a64d7764db322adce9f064ac
Vit Listik <vit.listik@firma.seznam.cz>
2016-04-25 15:24:52 +0200

    version 1.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='AerospikeClientMock',
-    version='1.0.1',
+    version='1.0.2',
     description='Aerospike client mock',
     long_description=
     """
